8586da44caefe64f0dad49739e5eb3ac0aad16c5
Shashwat Gupta <shashwatsg259@gmail.com>
2022-05-24 16:30:39 +0530

    longer text handled

--- script.js
+++ script.js
@@ -1,94 +1,102 @@
 import data from "./data.json" assert { type: "json" };
 
-const article = document.querySelector("article");
 var position = 0;
+var size;
+var threshold;
 
-//this function handles the string which is too long
-const limitLength = (item) => {
-  if (item.length <= 32) return item;
-  const newString = item.slice(0, 16) + "..." + item.slice(-16);
-  return newString;
+const limitlength = (item) => {
+  if (item.length < threshold) {
+    return item;
+  }
+  var newstr =
+    item.slice(0, (threshold - 3) / 2) +
+    "..." +
+    item.slice(-(threshold - 3 - (threshold - 3) / 2));
+  return newstr;
 };
-//creating a div element for every image item
+
+//creating the left column for the app
+const left_cont = document.querySelector(".leftContainer");
 data.forEach((item) => {
-  const newItem = document.createElement("div");
-  newItem.classList.add("innerDiv");
-  newItem.innerHTML = `
- <img src=${item.previewImage} alt="img" class="itemIMG">
-  <div>${limitLength(item.title)}</div>
-    `;
-  newItem.setAttribute("id", position);
-  position++;
-  article.append(newItem);
+  const newdiv = document.createElement("div");
+  newdiv.classList.add("innerDiv");
+  newdiv.setAttribute("tabindex", -1);
+  newdiv.setAttribute("id", position++);
+  newdiv.innerHTML = `
+  <img class="leftimg" alt="img" src="${item.previewImage}">
+  <div class="imagelabel"><div>
+  `;
+  left_cont.append(newdiv);
+  var style = window
+    .getComputedStyle(newdiv, null)
+    .getPropertyValue("font-size");
+  size = parseFloat(style);
 });
 
-const main = document.querySelector("main");
+//calculating the threshold string length
+const check = document.createElement("div");
+check.classList.add("check");
+check.style.fontSize = size + "px";
+check.style.whiteSpace = "nowrap";
+check.style.overflow = "hidden";
+check.style.width = "260px";
+document.querySelector("body").append(check);
+var str = "0";
+for (threshold = 1; threshold < 100; threshold++) {
+  check.innerText = str;
+  if (check.scrollWidth > check.clientWidth) {
+    break;
+  }
+  str += "0";
+}
+check.remove();
+
 position = 0;
-const figure = document.createElement("figure");
-figure.innerHTML = `
-<img src=${data[0].previewImage} alt="img" >`;
-main.append(figure);
+//creating the right column for the app
+const right_cont = document.querySelector(".rightContainer");
 
-const nameFigure = document.createElement("textarea");
-nameFigure.value = data[0].title;
-figure.append(nameFigure);
+right_cont.innerHTML = `
+<img src=${data[position].previewImage} class="rightimg" alt="img">
+<textarea class="caption" >${data[position].title}</textarea>
+`;
 
-const listitems = document.querySelectorAll("article .innerDiv");
-listitems[0].classList.add("selected");
+const listitems = left_cont.querySelectorAll(".innerDiv");
+listitems[0].focus();
 
-//adding click event to all the image items
+//adding click functionality
 listitems.forEach((item) => {
+  item.querySelector(".imagelabel").innerText = limitlength(
+    data[item.id].title
+  );
   item.addEventListener("click", (e) => {
     position = item.id;
-    listitems.forEach((item2) => {
-      item2.classList.remove("selected");
-    });
-    item.classList.add("selected");
-    figure
-      .querySelector("img")
-      .setAttribute("src", item.querySelector("img").src);
-    nameFigure.value = data[position].title;
+    right_cont
+      .querySelector(".rightimg")
+      .setAttribute("src", data[position].previewImage);
+    right_cont.querySelector(".caption").value = data[position].title;
   });
 });
 
-//handling the press upArrow event
-document.addEventListener("keydown", (e) => {
-  if (e.code == "ArrowUp") {
-    if (position > 0) {
-      position--;
-      listitems.forEach((item) => {
-        item.classList.remove("selected");
-      });
-      listitems[position].classList.add("selected");
-      figure
-        .querySelector("img")
-        .setAttribute("src", listitems[position].querySelector("img").src);
-      nameFigure.value = data[position].title;
-    }
-  }
-});
-
-//handling the press downArrow event
+//adding up and down functionality
 document.addEventListener("keydown", (e) => {
   if (e.code == "ArrowDown") {
-    if (position < listitems.length - 1) {
-      position++;
-      listitems.forEach((item) => {
-        item.classList.remove("selected");
-      });
-      listitems[position].classList.add("selected");
-      figure
-        .querySelector("img")
-        .setAttribute("src", listitems[position].querySelector("img").src);
-      nameFigure.value = data[position].title;
-    }
-  }
+    position++;
+    if (position == data.length) position = 0;
+  } else if (e.code == "ArrowUp") {
+    position--;
+    if (position < 0) position = data.length - 1;
+  } else return;
+  listitems[position].focus();
+  right_cont
+    .querySelector(".rightimg")
+    .setAttribute("src", data[position].previewImage);
+  right_cont.querySelector(".caption").value = data[position].title;
 });
 
-nameFigure.addEventListener("input", (e) => {
-  //handling the case when some change is made in the image name
-  listitems[position].querySelector("div").innerHTML = `${limitLength(
+const caption = right_cont.querySelector(".caption");
+caption.addEventListener("input", (e) => {
+  listitems[position].querySelector(".imagelabel").innerText = limitlength(
     e.target.value
-  )}`;
+  );
   data[position].title = e.target.value;
 });
